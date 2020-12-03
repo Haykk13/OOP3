68ff90a085829ce61826ce4036a828bea3f5e724
Hayk <hayk.hayr3@gmail.com>
2020-12-04 01:20:34 +0400

    Add project files.

---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OOP3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            MyClass myClass = new MyClass();
+
+            Console.WriteLine(myClass.X);
+        }
+    }
+
+    class MyClass
+    {
+        int x;
+
+        public int X { get { return x; } }
+        public MyClass() { }
+        public MyClass(int x)
+        {
+            this.x = x;
+        }
+    }
+}
